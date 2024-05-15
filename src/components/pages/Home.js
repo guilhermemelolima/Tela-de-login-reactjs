6cad354dfa82ee0d60f399c6e2616804313b935d
Guilherme Melo <limaguilherme.melo@gmail.com>
2024-05-15 09:19:23 -0300

    tela_login v_1.0

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,50 +5,40 @@ import { AuthContext } from "../../contexts/auth/AuthContext";
 import Button from '../layout/Button';
 
 import style from './Home.module.css';
-// import { useNavigate } from "react-router-dom";
 
-function Home(props) {
+function Home() {
 
     const auth = useContext(AuthContext);
-    // const navigate = useNavigate()
 
     async function handleLogout(){
         await auth.signout();
         window.location.reload();
-        // navigate('/login')
     }
 
     return (
         <section className={style.conteudo}>
-            {/* {console.log('form')}
-			{console.log('auth: ')}
-			{console.log(auth)}
-            {console.log('user: ')}
-            {console.log(auth.user)}
-            {console.log('token: ' )}
-            {console.log(auth.token)} */}
             <h2>Sejá bem vindo {auth.user.nome}</h2>
             <div className="imgPerfil">
-                <img src="https://placehold.co/100X100" alt={`Imagem de perfil de ${auth.user.nome}`}/>
+                <img src="https://placehold.co/100X100" alt={`Imagem de perfil de ${auth.user.name}`}/>
             </div>
 
             <ul>
                 <li>
                     <span>Nome: </span>
-                    <span>{auth.user.nome}</span>
+                    <span>{auth.user.name}</span>
 
                 </li>
                 <li>
                     <span>Email: </span>
-                    <span>{auth.user.nome}</span>
+                    <span>{auth.user.email}</span>
                 </li>
                 <li>
                     <span>Senha: </span>
-                    <span>{auth.user.nome}</span>
+                    <span>{auth.user.password}</span>
                 </li>
                 <li>
                     <span>Token: </span>
-                    <span>{auth.token}</span>
+                    <span>{localStorage.getItem('authToken')}</span>
                 </li>
                 {/* <li>
                     <span>Role: </span>
--- src/contexts/auth/AuthProvider.js
+++ src/contexts/auth/AuthProvider.js
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 import { AuthContext } from "./AuthContext"
 import { UseApi } from "../../hooks/UseApi";
@@ -9,23 +9,13 @@ function AuthProvider ({children}) {
     const [user, setUser] = useState(null);
     const api = UseApi();
 
-	// todo: erro no useEffect
+	// todo: useEffect com loop infinito quando adicionada a dependencia api
     useEffect(() => {
         async function validateToken(){
             const storageData = localStorage.getItem('authToken');
-			console.log('localStorage.getItem("authToken")')
-			console.log(localStorage.getItem('authToken'))
-			console.log("STORAGEDATA")
-			console.log(storageData)
             if (storageData) {
                 const data = await api.validateToken(storageData);
-					console.log('\nVALIDATE TOKEN IF STORAGEDATA\n')
-					console.log('token existe')
-					console.log(data.user)
                 if (data.user) {
-					console.log('\nVALIDATE TOKEN IF DATA.USER\n')
-					console.log('usuário existe')
-					console.log(data.user)
                     setUser(data.user);
                 }
             }
@@ -37,17 +27,7 @@ function AuthProvider ({children}) {
         const data = await api.signin(email, password);
         if (data.user && data.token) {
             setUser(data.user);
-            setToken(data.token)
-			console.log('\nAUTHPROVIDER DATA\n')
-			console.log('data.user: ')
-			console.log(data.user)
-			console.log('data.token: ')
-            console.log(data.token)
-			console.log('\nAUTHPROVIDER USER | TOKE\n')
-			console.log('user: ')
-			console.log(user)
-			console.log('token: ')
-            console.log(localStorage.getItem('authToken'))
+            setToken(data.token);
             return true;
         }
         return false;
@@ -56,7 +36,7 @@ function AuthProvider ({children}) {
     async function signout(){
         // await api.logout();
         setUser(null);
-        setToken('')
+        setToken('');
     }
 
     function setToken(token){
